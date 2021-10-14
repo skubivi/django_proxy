af73495d22a19a17012cbea2515b386e84fa3f16
Skubivi <altair100001@gmail.com>
2021-10-14 21:01:51 +0300

    final v4

--- src/djangoProject1/settings.py
+++ src/djangoProject1/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iqf782f==+@fep&!e_f0b0p*s1+l%7xp7w9j02-*92fo5pb40j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+]
 
 
 # Application definition
